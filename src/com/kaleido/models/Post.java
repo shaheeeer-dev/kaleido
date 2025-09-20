c89bf9c6226841e96477718b659cba1379f47223
Muhammad Shaheer <shaheeramazon1@gmail.com>
2025-09-21 02:42:28 +0500

    Post class complete

--- src/com/kaleido/models/Post.java
+++ src/com/kaleido/models/Post.java
@@ -1,4 +1,73 @@
 package com.kaleido.models;
 
+import java.time.LocalDateTime;
+
 public class Post {
+    private int postId;
+    private int userId;
+    private int communityId;
+    private String contentText;
+    private String imageUrl;
+    private LocalDateTime createdAt;
+
+
+    public Post() {
+    }
+
+    public Post(int postId, int userId, int communityId, String contentText, String imageUrl, LocalDateTime createdAt) {
+        this.postId = postId;
+        this.userId = userId;
+        this.communityId = communityId;
+        this.contentText = contentText;
+        this.imageUrl = imageUrl;
+        this.createdAt = createdAt;
+    }
+
+    public int getPostId() {
+        return postId;
+    }
+
+    public void setPostId(int postId) {
+        this.postId = postId;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public int getCommunityId() {
+        return communityId;
+    }
+
+    public void setCommunityId(int communityId) {
+        this.communityId = communityId;
+    }
+
+    public String getContentText() {
+        return contentText;
+    }
+
+    public void setContentText(String contentText) {
+        this.contentText = contentText;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
 }
