64cd44ac2356457878506f59c85309c0aee6eac1
Muhammad Shaheer <shaheeramazon1@gmail.com>
2025-09-23 00:11:18 +0500

    merging user-post and db

---
+++ src/com/kaleido/db/DatabaseConnection.java
@@ -0,0 +1,20 @@
+package com.kaleido.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DatabaseConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/kaleido";
+    private static final String USER = "root";  // Default XAMPP username
+    private static final String PASSWORD = "";  // Default XAMPP password (empty)
+
+    public static Connection getConnection() throws SQLException {
+        try {
+            Class.forName("oracle.jdbc.driver.OracleDriver");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        return DriverManager.getConnection(URL, USER, PASSWORD);
+    }
+}
