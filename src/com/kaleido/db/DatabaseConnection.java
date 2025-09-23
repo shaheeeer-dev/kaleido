257802437404d17c3ca528c6f1c307290786440c
Muhammad Shaheer <shaheeramazon1@gmail.com>
2025-09-23 16:21:12 +0500

    connection maintained

--- src/com/kaleido/db/DatabaseConnection.java
+++ src/com/kaleido/db/DatabaseConnection.java
@@ -6,8 +6,8 @@ import java.sql.SQLException;
 
 public class DatabaseConnection {
     private static final String URL = "jdbc:mysql://localhost:3306/kaleido";
-    private static final String USER = "root";  // Default XAMPP username
-    private static final String PASSWORD = "";  // Default XAMPP password (empty)
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
 
     public static Connection getConnection() throws SQLException {
         try {
