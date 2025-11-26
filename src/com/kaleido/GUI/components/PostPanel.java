c76e2caf20bc760ecd02a4a98f7f12fca43c82d4
Muhammad Shaheer <shaheeramazon1@gmail.com>
2025-11-26 20:13:25 +0500

    posts fixed size and panel created

--- src/com/kaleido/GUI/components/PostPanel.java
+++ src/com/kaleido/GUI/components/PostPanel.java
@@ -1,4 +1,35 @@
 package com.kaleido.GUI.components;
 
-public class PostPanel {
+import com.kaleido.models.Post;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class PostPanel extends JPanel {
+
+    public PostPanel(Post post) {
+        setLayout(new BorderLayout());
+        setBackground(new Color(25, 25, 27));
+        setBorder(BorderFactory.createCompoundBorder(
+                BorderFactory.createMatteBorder(1, 0, 1, 0, Color.GRAY),
+                BorderFactory.createEmptyBorder(10, 20, 10, 20)
+        ));
+        setMaximumSize(new Dimension(Integer.MAX_VALUE, 150));
+
+        JLabel userLabel = new JLabel(post.getUsername() + "    " + post.getCreatedAt());
+        userLabel.setForeground(Color.LIGHT_GRAY);
+        userLabel.setFont(new Font("SansSerif", Font.PLAIN, 12));
+
+        JTextArea contentArea = new JTextArea(post.getContentText());
+        contentArea.setLineWrap(true);
+        contentArea.setWrapStyleWord(true);
+        contentArea.setEditable(false);
+        contentArea.setBackground(new Color(25, 25, 27));
+        contentArea.setForeground(Color.WHITE);
+        contentArea.setFont(new Font("SansSerif", Font.PLAIN, 14));
+
+
+        add(userLabel, BorderLayout.NORTH);
+        add(contentArea, BorderLayout.CENTER);
+    }
 }
